37c506b593bebb54999741a28c6452692e193d1d
Brendan McBride <brendan.mcmath@gmail.com>
2022-09-22 12:09:12 +0800

    Create a fetch request to get the json for search results from the MealDB API

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const search = document.getElementById("search");
 const submit = document.getElementById("submit");
 const random = document.getElementById("random");
 const result_heading = document.getElementById("result-heading");
+const meals_el = document.getElementById("meals");
 const single_meal_el = document.getElementById("single-meal");
 
 // Search meal and fetch from API
@@ -20,7 +21,26 @@ function search_meals(e) {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        result_heading.innerHTML = `<h2>Search Results for ${search_term}: </h2>`;
+
+        if (data.meals == null) {
+          result_heading.innerHTML = `<p>There are no results matching ${search_term}</p>`;
+        } else {
+          meals_el.innerHTML = data.meals
+            .map(
+              (meal) =>
+                `<div class="meal">
+              <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+              <div class="meal-info" data-mealID="${meal.idMeal}">
+                <h3 class="meal-title">${meal.strMeal}</h3>
+              </div>
+            </div>`
+            )
+            .join("");
+        }
       });
+    // Clear search text
+    search.value = "";
   } else {
     alert("Please enter a valid search term before submitting");
   }
