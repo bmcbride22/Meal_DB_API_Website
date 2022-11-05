f3d7e5ea8b39e0ea54ddf1c685d61227b182232a
Brendan McBride <brendan.mcmath@gmail.com>
2022-11-05 14:54:02 +0800

    add ESlint config to file

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const search = document.getElementById("search");
-const submit = document.getElementById("submit");
-const random = document.getElementById("random");
-const result_heading = document.getElementById("result-heading");
-const meals_el = document.getElementById("meals");
-const single_meal_el = document.getElementById("single-meal");
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const random = document.getElementById('random');
+const result_heading = document.getElementById('result-heading');
+const meals_el = document.getElementById('meals');
+const single_meal_el = document.getElementById('single-meal');
 
 // Search meal and fetch from API
 function search_meals(e) {
   e.preventDefault();
 
-  //Clear single meal
-  single_meal_el.innerHTML = "";
+  // Clear single meal
+  single_meal_el.innerHTML = '';
 
   // Get search term
   const search_term = search.value;
@@ -28,21 +28,20 @@ function search_meals(e) {
         } else {
           meals_el.innerHTML = data.meals
             .map(
-              (meal) =>
-                `<div class="meal">
+              (meal) => `<div class="meal">
                   <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
                   <div class="meal-info" data-mealID="${meal.idMeal}">
                     <h3 class="meal-title">${meal.strMeal}</h3>
                   </div>
-                </div>`
+                </div>`,
             )
-            .join("");
+            .join('');
         }
       });
     // Clear search text
-    search.value = "";
+    search.value = '';
   } else {
-    alert("Please enter a valid search term before submitting");
+    alert('Please enter a valid search term before submitting');
   }
 }
 
@@ -62,9 +61,9 @@ function get_meal_by_id(meal_id) {
 
 function get_random_meal() {
   // Clear meals and heading
-  meals_el.innerHTML = "";
-  result_heading.innerHTML = "";
-  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+  meals_el.innerHTML = '';
+  result_heading.innerHTML = '';
+  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
     .then((res) => res.json())
     .then((data) => {
       const meal = data.meals[0];
@@ -79,7 +78,7 @@ function add_meal_to_DOM(meal) {
   for (let i = 1; i <= 20; i++) {
     if (meal[`strIngredient${i}`]) {
       ingredients.push(
-        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`,
       );
     } else {
       break;
@@ -91,18 +90,17 @@ function add_meal_to_DOM(meal) {
       <h1>${meal.strMeal}</h1>
       <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
       <div class="single-meal-info">
-        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
-        ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
       </div>
       <div class="main">
       <h2 class="ingredients-title">
           <ul class="ingredient-list">
             ${ingredients
-              .map(
-                (ingredient) =>
-                  `<li class="ingredient-list__item">${ingredient}</li>`
-              )
-              .join("")}
+    .map(
+      (ingredient) => `<li class="ingredient-list__item">${ingredient}</li>`,
+    )
+    .join('')}
           </ul>
         </h2>
         <p class="instructions">${meal.strInstructions}</p>
@@ -113,24 +111,23 @@ function add_meal_to_DOM(meal) {
 
 // Event Listeners //
 
-submit.addEventListener("submit", search_meals);
-random.addEventListener("click", get_random_meal);
+submit.addEventListener('submit', search_meals);
+random.addEventListener('click', get_random_meal);
 
 // Add event listener to the conatiner of each meal to get the
 // data-mealid when thumbnail clicked on
 
 // Need to find out if the meal info div belongs to the element we clicked
 
-meals_el.addEventListener("click", (e) => {
+meals_el.addEventListener('click', (e) => {
   const meal_info = e.path.find((item) => {
     if (item.classList) {
-      return item.classList.contains("meal-info");
-    } else {
-      return false;
+      return item.classList.contains('meal-info');
     }
+    return false;
   });
   if (meal_info) {
-    const meal_id = meal_info.getAttribute("data-mealid");
+    const meal_id = meal_info.getAttribute('data-mealid');
     get_meal_by_id(meal_id);
   }
 });
